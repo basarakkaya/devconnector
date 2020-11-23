799cbcddebf8da3a4595fe243a9338b608150ffd
Osman Başar Akkaya <akkayabasar@gmail.com>
2020-11-23 10:46:43 +0300

    create redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+
+import './App.css';
 
 const App = () => {
   return (
