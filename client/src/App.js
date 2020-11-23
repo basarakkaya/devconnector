a6829e6f70812c8537ae18f572ffe5454b33882f
Osman Başar Akkaya <akkayabasar@gmail.com>
2020-11-23 13:35:14 +0300

    load user & set auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -13,6 +14,10 @@ import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
