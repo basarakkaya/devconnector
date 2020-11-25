b26e88a8f2fa537528155722ea88ba04fa32d709
Osman Başar Akkaya <akkayabasar@gmail.com>
2020-11-25 18:11:20 +0300

    fix: Not redirecting when logout from profiles path issue

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
         </Link>
       </li>
       <li>
-        <a onClick={logout} href='#!'>
+        <a onClick={logout} href='/#!'>
           <i className=' fas fa-sign-out-alt'></i>{' '}
           <span className='hide-sm'>Logout</span>
         </a>
